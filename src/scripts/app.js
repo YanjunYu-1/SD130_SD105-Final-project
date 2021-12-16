27b24838d1aedea3ae2c836f80c0026e72081266
Bob yu <y9y9j8@163.com>
2021-12-16 14:41:44 -0600

    add information

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,15 @@
 import {apiKey, bbox, wtApiKey} from './modules/rul.js'
-import {originForm,destinationForm,startingLocationsElement,destinationsElement} from './modules/node.js'
+import {originForm,destinationForm,startingLocationsElement,destinationsElement,planTripButton} from './modules/node.js'
 
 startingLocationsElement.innerHTML='';
 destinationsElement.innerHTML='';
+
+document.querySelector('.my-trip').innerHTML = '';
+document.querySelector('.my-trip').insertAdjacentHTML(
+        'afterbegin',
+		`<div id="mywarn" style="color:#F00"></div>`
+);
+
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
--- src/scripts/modules/node.js
+++ src/scripts/modules/node.js
@@ -2,3 +2,4 @@ export const originForm = document.querySelector('.origin-form');
 export const destinationForm = document.querySelector('.destination-form')
 export const startingLocationsElement = document.querySelector('.origins');
 export const destinationsElement = document.querySelector('.destinations');
+export const planTripButton = document.querySelector('.plan-trip');
